f5d70e21a5a40d41c6fd7439a584fb91d0a40e3e
uKeversu <109996005+uKeversu@users.noreply.github.com>
2024-01-26 16:35:51 -0300

    Aula 3
    Tratar erros com try catch.

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-  .then((res) => res.json())
-  .then((videos) =>
+async function buscarEMostrarVideos() {
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
+
     videos.forEach((video) => {
+      if (video.categoria === "") {
+        throw new error("Vídeo não possui categoria.");
+      }
       containerVideos.innerHTML += `
-      <li class="videos__item">
-      <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-        <div class="descricao-video">
-        <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-        <h3 class="titulo-video">${video.titulo}</h3>
-        <p class="titulo-canal">${video.descricao}</p>
-        </div>
-    </li>`;
-    })
-  )
-  .catch((error) => {
-    containerVideos.innerHTML = `<p>Houve um erro ao carregar o conteúdo dos vídeos:${error}`;
-  });
+                <li class="videos__item">
+                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="descricao-video">
+                                <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                                <h3 class="titulo-video">${video.titulo}</h3>
+                                <p class="titulo-canal">${video.descricao}</p>
+                                <p class="categoria" hidden>${video.categoria}</p>
+                        </div>
+                </li>
+                `;
+    });
+  } catch (error) {
+    containerVideos.innerHTML = `<h4> Houve um erro ao carregar os vídeos: ${error}</4>`;
+  }
+}
+
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector("pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll("videos__item");
+  if (barraDePesquisa.value != "") {
+  } else {
+  }
+}
