b2c942a7e1e4f1bcdb472755fd7da4ba77c13e31
uKeversu <109996005+uKeversu@users.noreply.github.com>
2024-01-29 10:24:08 -0300

    Aula 4
    Criar filtragem dos vídeos por categoria.

--- script.js
+++ script.js
@@ -28,13 +28,44 @@ async function buscarEMostrarVideos() {
 
 buscarEMostrarVideos();
 
-const barraDePesquisa = document.querySelector("pesquisar__input");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
 function filtrarPesquisa() {
-  const videos = document.querySelectorAll("videos__item");
-  if (barraDePesquisa.value != "") {
-  } else {
+  const videos = document.querySelectorAll(".videos__item");
+  const valorFiltro = barraDePesquisa.value.toLowerCase();
+
+  videos.forEach((video) => {
+    const titulo = video
+      .querySelector(".titulo-video")
+      .textContent.toLowerCase();
+
+    video.style.display = valorFiltro
+      ? titulo.includes(valorFiltro)
+        ? "block"
+        : "none"
+      : "block";
+  });
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+  let nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+  const videos = document.querySelectorAll(".videos__item");
+  for (let video of videos) {
+    let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    let valorFiltro = filtro.toLowerCase();
+
+    if (!categoria.includes(valorFiltro) && valorFiltro != "tudo") {
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
+    }
   }
 }
